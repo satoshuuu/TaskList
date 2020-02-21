a0e29b3dc6a5477373923670447adcd45c4443da
shuhei.sato <s.s.odn.0617@gmail.com>
2020-02-21 01:13:06 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -43,8 +43,12 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-        $task = new Task;
+        $this->validate($request, [
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
+            ]);
         
+        $task = new Task;
         $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
@@ -90,6 +94,11 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
+            ]);
+        
         $task = Task::find($id);
         $task->status = $request->status;
         $task->content = $request->content;
