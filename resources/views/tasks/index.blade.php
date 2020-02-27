c61e2eb74c6c5cdd4516b3799ab7ae335a8f77f6
shuhei.sato <s.s.odn.0617@gmail.com>
2020-02-27 12:04:19 +0000

    user login

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,30 +2,39 @@
 
 @section('content')
 
-    <h1>タスク一覧</h1>
+    @if(Auth::check())
+        <h1>タスク一覧</h1>
     
-    @if (count($tasks) > 0) 
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>タスク</th>
-                    <th>ステータス</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                    <td>{{ $task->content }}</td>
-                    <td>{{ $task->status }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        @if (count($tasks) > 0) 
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>タスク</th>
+                        <th>ステータス</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                        <td>{{ $task->content }}</td>
+                        <td>{{ $task->status }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+        
+        {!! link_to_route('tasks.create', 'タスクの新規作成', [], ['class' => 'btn btn-primary']) !!}
+        
+    @else
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the Tasklist</h1>
+            {!! link_to_route('signup.get', 'Sign up now', [], ['class' => 'btn btn-lg btn-primary']) !!}
+        </div>
+    </div>
     @endif
-    
-    {!! link_to_route('tasks.create', 'タスクの新規作成', [], ['class' => 'btn btn-primary']) !!}
-    
 
 @endsection
